96e6eb17544902c3ceef547c43fc362958f7865c
denyyok1995 <denyok@naver.com>
2022-08-13 14:13:33 +0900

    순환 참조로 인한 @Bean 생성 위치 변경

--- src/main/java/com/pjt/globalmarket/CoupangApplication.java
+++ src/main/java/com/pjt/globalmarket/CoupangApplication.java
@@ -2,9 +2,16 @@ package com.pjt.globalmarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class CoupangApplication {
+
+    @Bean
+    protected BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CoupangApplication.class, args);
