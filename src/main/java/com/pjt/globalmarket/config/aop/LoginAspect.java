a01e094e603c9225522b01fb1ee58bc7d6d61dd1
denyyok1995 <denyok@naver.com>
2022-08-24 20:17:57 +0900

    로그인 확인 AOP 로직 수정

--- src/main/java/com/pjt/globalmarket/config/aop/LoginAspect.java
+++ src/main/java/com/pjt/globalmarket/config/aop/LoginAspect.java
@@ -1,6 +1,8 @@
 package com.pjt.globalmarket.config.aop;
 
 import com.pjt.globalmarket.config.auth.UserAuthDetails;
+import com.pjt.globalmarket.user.service.UserService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
@@ -14,16 +16,23 @@ import java.util.List;
 @Slf4j
 @Aspect
 @Component
+@RequiredArgsConstructor
 public class LoginAspect {
+
+    private UserService userService;
 
     //@Before(value = "@annotation(com.pjt.globalmarket.user.domain.NeedLogin)")
     public void checkLogin(JoinPoint joinPoint) {
         List<Object> args = Arrays.asList(joinPoint.getArgs());
-        log.info("Login Check");
         for(Object o : args) {
             if(o instanceof UserAuthDetails) {
-                log.info("로그인 된 유저입니다.");
-                return;
+                UserAuthDetails user = (UserAuthDetails) o;
+                if(userService.getActiveUserByEmailAndProvider(user.getUsername(), user.getProvider()).isEmpty()) {
+                    throw new AuthenticationCredentialsNotFoundException("로그인 해야 합니다.");
+                } else {
+                    log.info("로그인 된 유저입니다.");
+                    return;
+                }
             }
         }
         throw new AuthenticationCredentialsNotFoundException("로그인 해야 합니다.");
