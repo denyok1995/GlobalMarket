36195b4af372fe6e0b0dd738a0b56b2aea65bd72
denyyok1995 <denyok@naver.com>
2022-08-28 19:01:43 +0900

    #7 manager constant 추가

--- src/main/java/com/pjt/globalmarket/user/domain/UserConstant.java
+++ src/main/java/com/pjt/globalmarket/user/domain/UserConstant.java
@@ -3,4 +3,6 @@ package com.pjt.globalmarket.user.domain;
 public class UserConstant {
 
     public static final String DEFAULT_PROVIDER = "GM";
+
+    public static final String MANAGER = "manager";
 }
