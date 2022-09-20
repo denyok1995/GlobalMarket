e1a340b579fd742d9dab2b8d95d9341cfe3e0f6b
denyyok1995 <denyok@naver.com>
2022-09-21 05:21:30 +0900

    #5 paging size min max 제한 추가

--- src/main/java/com/pjt/globalmarket/product/controller/ProductController.java
+++ src/main/java/com/pjt/globalmarket/product/controller/ProductController.java
@@ -16,6 +16,9 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
 @RestController
 @RequiredArgsConstructor
 @Slf4j
@@ -23,16 +26,14 @@ public class ProductController {
 
     private final ProductService productService;
 
-    // NOTE: 만일 page=0 또는 size=1000000 와 같이 큰 숫자를 넣으면 어떻게 될까요?
     // 특정 카테고리에 있는 제품만 목록을 표시하고 싶어요, 또는 특정 카테고리(신발)에 포함된 제품중 "나이키" 제품을 찾고 싶어요.
     // 이런 조건들은 어떻게 처리해야 할까요?
-
     @GetMapping(path = "/products")
     @ApiOperation(value = "상품 검색", notes = "입력(content)에 일치하는 상품을 조회한다.")
     public PageList searchProducts(@AuthenticationPrincipal @ApiIgnore UserAuthDetails loginUser,
                                    @RequestParam(required = false) String content,
                                    @RequestParam int page,
-                                   @RequestParam int size) {
+                                   @RequestParam @Min(20) @Max(50) int size) {
         PageRequest pageRequest = PageRequest.of(page, size);
         PageList products;
         if(content == null) {
