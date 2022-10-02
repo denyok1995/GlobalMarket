68a4fdb4da6a6f8eef0f94a04898dc03ee1c9f7a
denyyok1995 <denyok@naver.com>
2022-10-02 19:05:27 +0900

    #15 쿠폰 통합으로 인한 Dto 생성

--- src/main/java/com/pjt/globalmarket/coupon/dto/CouponDto.java
+++ src/main/java/com/pjt/globalmarket/coupon/dto/CouponDto.java
@@ -1,12 +1,15 @@
 package com.pjt.globalmarket.coupon.dto;
 
-import com.pjt.globalmarket.coupon.domain.Coupon;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.pjt.globalmarket.coupon.domain.CouponType;
+import com.pjt.globalmarket.product.dto.ProductResponseDto;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import io.swagger.annotations.ApiOperation;
 import lombok.*;
 
 import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 @NoArgsConstructor
@@ -15,37 +18,62 @@ import java.time.ZonedDateTime;
 @ApiModel(description = "쿠폰 정보")
 public class CouponDto {
 
-    @ApiModelProperty(notes = "쿠폰 고유 아이디", example = "2")
+    @ApiModelProperty(notes = "쿠폰 고유 아이디", example = "10")
     private long id;
 
-    @ApiModelProperty(notes = "쿠폰 이름", example = "3월 봄맞이 할인 쿠폰", required = true)
+    @ApiModelProperty(notes = "쿠폰 종류", example = "PRICE")
+    private CouponType couponType;
+
+    @ApiModelProperty(notes = "보여줄 쿠폰 이름", example = "3월 봄맞이 할인 쿠폰")
     private String name;
 
-    @ApiModelProperty(notes = "쿠폰 사용을 위한 최소 금액", example = "20000.0", required = true)
+    @ApiModelProperty(notes = "쿠폰 식별 이름", example = "2022년 1월 신규 회원 쿠폰")
+    private String couponId;
+
+    @ApiModelProperty(notes = "쿠폰 사용을 위한 최소 금액", example = "20000.0")
     private double minPrice;
 
-    @ApiModelProperty(notes = "할인 금액", example = "1000.0", required = false)
+    @ApiModelProperty(notes = "할인 금액", example = "1000.0")
     private double discountPrice;
 
-    @ApiModelProperty(notes = "할인 율", example = "10", required = false)
+    @ApiModelProperty(notes = "할인 율", example = "10")
     private double discountPercent;
 
-    @ApiModelProperty(notes = "최대 할인 금액", example = "2000원", required = true)
+    @ApiModelProperty(notes = "최대 할인 금액", example = "2000.0")
     private double maxDiscountPrice;
 
-    //private Long productId;
+    @ApiModelProperty(notes = "최소 상품 개수", example = "3")
+    private long minProductCount;
 
-    @ApiModelProperty(notes = "최대 발급 개수", example = "300000", required = true)
+    @ApiModelProperty(notes = "할인이 적용되는 상품들", example = "[1,2,4]")
+    private List<ProductResponseDto> products;
+
+    @ApiModelProperty(notes = "최대 발급 개수", example = "300000")
     private long maxCouponCount;
 
+    @ApiModelProperty(notes = "중복 사용 가능 여부", example = "true")
+    private boolean overlap;
 
-    public static CouponDto toDto(Coupon coupon) {
+    @ApiModelProperty(notes = "쿠폰 만료 기간", example = "2023-10-01 14:20:43")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private ZonedDateTime expiredDate;
+
+
+    public static CouponDto toDto(IntegratedCoupon coupon) {
         return CouponDto.builder()
                 .id(coupon.getId())
+                .couponType(coupon.getCouponType())
                 .name(coupon.getName())
+                .couponId(coupon.getCouponId())
                 .minPrice(coupon.getMinPrice())
                 .discountPrice(coupon.getDiscountPrice())
+                .discountPercent(coupon.getDiscountPercent())
+                .maxDiscountPrice(coupon.getMaxDiscountPrice())
+                .minProductCount(coupon.getMinProductCount())
+                .products(coupon.getProducts().stream().map(product -> ProductResponseDto.toDto(product, 1.0)).collect(Collectors.toList()))
                 .maxCouponCount(coupon.getMaxCouponCount())
+                .overlap(coupon.isOverlap())
+                .expiredDate(coupon.getExpiredDate())
                 .build();
     }
 }
---
+++ src/main/java/com/pjt/globalmarket/coupon/dto/IntegratedCoupon.java
@@ -0,0 +1,101 @@
+package com.pjt.globalmarket.coupon.dto;
+
+import com.pjt.globalmarket.coupon.domain.*;
+import com.pjt.globalmarket.product.domain.Product;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.*;
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class IntegratedCoupon {
+
+    private long id;
+
+    private CouponType couponType;
+
+    private String name;
+
+    private String couponId;
+
+    private double minPrice;
+
+    private double discountPrice;
+
+    private double discountPercent;
+
+    private double maxDiscountPrice;
+
+    private long minProductCount;
+
+    @Builder.Default
+    private List<Product> products = new ArrayList<>();
+
+    private long maxCouponCount;
+
+    private boolean overlap;
+
+    private ZonedDateTime expiredDate;
+
+
+    public static IntegratedCoupon toDto(PriceCoupon priceCoupon) {
+        return IntegratedCoupon.builder()
+                .id(priceCoupon.getId())
+                .couponType(CouponType.PRICE)
+                .couponId(priceCoupon.getCouponId())
+                .name(priceCoupon.getName())
+                .minPrice(priceCoupon.getMinPrice())
+                .discountPrice(priceCoupon.getDiscountPrice())
+                .maxCouponCount(priceCoupon.getMaxCouponCount())
+                .expiredDate(priceCoupon.getExpiredDate())
+                .build();
+    }
+
+    public static IntegratedCoupon toDto(PercentCoupon percentCoupon) {
+        return IntegratedCoupon.builder()
+                .id(percentCoupon.getId())
+                .couponType(CouponType.PERCENT)
+                .couponId(percentCoupon.getCouponId())
+                .name(percentCoupon.getName())
+                .discountPercent(percentCoupon.getDiscountPercent())
+                .maxDiscountPrice(percentCoupon.getMaxDiscountPrice())
+                .maxCouponCount(percentCoupon.getMaxCouponCount())
+                .expiredDate(percentCoupon.getExpiredDate())
+                .build();
+    }
+
+    public static IntegratedCoupon toDto(ProductCoupon productCoupon) {
+        return IntegratedCoupon.builder()
+                .id(productCoupon.getId())
+                .couponType(CouponType.PRODUCT)
+                .couponId(productCoupon.getCouponId())
+                .name(productCoupon.getName())
+                .discountPercent(productCoupon.getDiscountPercent())
+                .minProductCount(productCoupon.getMinProductCount())
+                .products(productCoupon.getProducts())
+                .maxCouponCount(productCoupon.getMaxCouponCount())
+                .expiredDate(productCoupon.getExpiredDate())
+                .build();
+    }
+
+    public static IntegratedCoupon toDto(CartCoupon cartCoupon) {
+        return IntegratedCoupon.builder()
+                .id(cartCoupon.getId())
+                .couponType(CouponType.CART)
+                .couponId(cartCoupon.getCouponId())
+                .name(cartCoupon.getName())
+                .discountPercent(cartCoupon.getDiscountPercent())
+                .minPrice(cartCoupon.getMinPrice())
+                .maxDiscountPrice(cartCoupon.getMaxDiscountPrice())
+                .overlap(cartCoupon.isOverlap())
+                .maxCouponCount(cartCoupon.getMaxCouponCount())
+                .build();
+    }
+}
