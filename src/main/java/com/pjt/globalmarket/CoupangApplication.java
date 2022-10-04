eea0b3e81541cba5658d37355ba8c4f4f823f45a
denyyok1995 <denyok@naver.com>
2022-10-04 20:09:05 +0900

    #15 Scheduling 사용하기 위해 어노테이션 추가

--- src/main/java/com/pjt/globalmarket/CoupangApplication.java
+++ src/main/java/com/pjt/globalmarket/CoupangApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
+@EnableScheduling
 public class CoupangApplication {
 
     @Bean
